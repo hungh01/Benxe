a7074dcc72ad08c66b7fa58a144a17e49a7fccdf
hungh01 <duyhung1632002@gmail.com>
2023-06-05 10:54:35 +0700

    5/6

--- src/main/java/com/example/BenXe/BenxeApplication.java
+++ src/main/java/com/example/BenXe/BenxeApplication.java
@@ -1,13 +1,11 @@
+package com.example.BenXe
 
-package com.example.BenXe;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public class BenxeApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BenxeApplication.class, args);
+object BenxeApplication {
+    fun main(args: Array<String?>?) {
+        SpringApplication.run(BenxeApplication::class.java, args)
     }
 }
